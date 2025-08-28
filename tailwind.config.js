c543a3973a5d999a2f182684903291f824880fb2
Bofi1 <angelborque03github@gmail.com>
2025-08-27 21:28:16 -0700

    mobile 1st part

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        JosefinSans: ["'Josefin+Sans'", "sans-serif"],
+      },
+      backgroundImage: {
+        gradientMain:
+          "linear-gradient(135deg, hsl(0, 80%, 86%),hsl(0, 74%, 74%))",
+
+        gradientHover:
+          "linear-gradient(135deg,hsl(0, 0%, 100%), hsl(0, 100%, 98%))",
+      },
+    },
+  },
+  plugins: [],
+};
